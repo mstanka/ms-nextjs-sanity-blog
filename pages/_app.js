abf48152a879db9e301074d7f09bee1593299f46
mstanka <mice78.mm@gmail.com>
2020-09-28 19:28:48 +0200

    add filtering menu with fontawesome icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { faList, faBorderAll } from "@fortawesome/free-solid-svg-icons";
+
+config.autoAddCss = false;
+library.add(faList, faBorderAll);
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import "styles/globals.css";
-import 'highlight.js/styles/atom-one-dark.css'
+import "highlight.js/styles/atom-one-dark.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "styles/index.scss";
 

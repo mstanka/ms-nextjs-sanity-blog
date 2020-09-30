3808acc790141b0fb43cba12ed14071c3c9058e1
mstanka <mice78.mm@gmail.com>
2020-09-30 22:03:10 +0200

    add pagination

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import PageLayout from "components/PageLayout";
 import AuthorIntro from "components/AuthorIntro";
-import CardItem from "components/CardItem";
-import CardListItem from "components/CardListItem";
 import FilteringMenu from "components/FilteringMenu";
 
+import { useGetBlogsPages } from "actions/pagination";
 import { getAllBlogs } from "lib/api";
-import { useGetBlogs} from "actions";
 
-export default function Home({ blogs: initialData }) {
+export default function Home({ blogs }) {
   const [filter, setFilter] = useState({
     view: { list: 0 },
   });
 
-  const { data: blogs, error } = useGetBlogs(initialData);
+  const { pages, isLoadingMore, isReachingEnd, loadMore } = useGetBlogsPages({
+    blogs,
+    filter,
+  });
 
   return (
     <PageLayout>
@@ -26,38 +27,7 @@ export default function Home({ blogs: initialData }) {
         }}
       />
       <hr />
-      <Row className="mb-5">
-        {blogs.map((blog) =>
-          !filter.view.list ? (
-            <Col key={`${blog.slug}-list`} md="9">
-              <CardListItem
-                author={blog.author}
-                title={blog.title}
-                subtitle={blog.subtitle}
-                date={blog.date}
-                link={{
-                  href: "/blogs/[slug]",
-                  as: `/blogs/${blog.slug}`,
-                }}
-              />
-            </Col>
-          ) : (
-            <Col key="blog.slug" md="4">
-              <CardItem
-                author={blog.author}
-                title={blog.title}
-                subtitle={blog.subtitle}
-                date={blog.date}
-                image={blog.coverImage}
-                link={{
-                  href: "/blogs/[slug]",
-                  as: `/blogs/${blog.slug}`,
-                }}
-              />
-            </Col>
-          )
-        )}
-      </Row>
+      <Row className="mb-5">{pages}</Row>
     </PageLayout>
   );
 }
@@ -66,7 +36,7 @@ export default function Home({ blogs: initialData }) {
 // provides props to your page
 // creates static page
 export async function getStaticProps() {
-  const blogs = await getAllBlogs({offset: 1});
+  const blogs = await getAllBlogs({ offset: 1 });
   return {
     props: {
       blogs,
